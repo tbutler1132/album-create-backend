50e2fa5ab2d3adbf11ec08eb69ee6ba2dddd02d8
github username <github email address>
2020-11-04 18:44:14 -0500

    AFtermvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,10 +42,10 @@ master_2 = Master.create(mix: mix_1, user: user_kanye, selected: false)
 #
 song_2 = Song.create(title: "Track 2", placement: 2, phase: 2, album: album_1)
 
-song_2_ref_img_1 = RefImg.create(title: "Kid Cudi", img_url: "https://static.hollywoodreporter.com/wp-content/uploads/2020/10/GettyImages-1210689746-H-2020-1603925642-928x523.jpg", song: song_2, user: user_kanye, selected: true)
-song_2_ref_img_2 = RefImg.create(title: "Kid Cudi 2", img_url: "https://static.billboard.com/files/2020/09/Kid-Cudi-jan-17-2020-a-billboard-1548-1601416648.jpg", song: song_2, user: user_tim, selected: false)
-song_2_ref_img_3 = RefImg.create(title: "Kid Cudi 3", img_url: "https://pyxis.nymag.com/v1/imgs/bcc/652/1fd748335749e3782b4c1eb8bf81e52a5b-19-kid-cudi.rsquare.w1200.jpg", song: song_2, user: user_mike_dean, selected: false)
-song_2_ref_img_4 = RefImg.create(title: "Kid Cudi 4", img_url: "https://img.washingtonpost.com/rf/image_1484w/WashingtonPost/Content/Blogs/therootdc/Images/World_Premiere_of_A_Haunted_House_064e4-681.jpg?uuid=V7vvqmCzEeKZQG_EiPP-zQ", song: song_2, user: user_mike_dean, selected: false)
+song_2_ref_img_1 = RefImg.create(title: "Desert", img_url: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/desert-manar-hawsawi.jpg", song: song_2, user: user_kanye, selected: true)
+song_2_ref_img_2 = RefImg.create(title: "Desert 2", img_url: "https://images.fineartamerica.com/images-medium-large-5/sonoran-desert-glow-harriet-peck-taylor.jpg", song: song_2, user: user_tim, selected: false)
+song_2_ref_img_3 = RefImg.create(title: "Desert 3", img_url: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/colorful-desert-bekim-art.jpg", song: song_2, user: user_mike_dean, selected: false)
+song_2_ref_img_4 = RefImg.create(title: "Desert 4", img_url: "https://cdn.shopify.com/s/files/1/1347/9399/products/Desert_Day.jpg?v=1546411143", song: song_2, user: user_mike_dean, selected: false)
 
 song_2_beat_1 = Beat.create(bpm: 92, key_sig: "C# minor", selected: false, song: song_2, user: user_tim)
 song_2_beat_2 = Beat.create(bpm: 90, key_sig: "F# minor", selected: false, song: song_2, user: user_kanye)
@@ -54,9 +54,15 @@ song_2_beat_3 = Beat.create(bpm: 189, key_sig: "D major", selected: false, song:
 
 song_3 = Song.create(title: "Track 3", placement: 3, phase: 1, album: album_1)
 
-song_3_ref_img_1 = RefImg.create(title: "Eminem", img_url: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Eminem_-_Concert_for_Valor_in_Washington%2C_D.C._Nov._11%2C_2014_%282%29_%28Cropped%29.jpg", song: song_3, user: user_kanye, selected: true)
-song_3_ref_img_2 = RefImg.create(title: "Marshall", img_url: "https://static.billboard.com/files/2020/11/Eminem-feb-2020-a-billboard-1548-1604341302-compressed.jpg", song: song_3, user: user_tim, selected: false)
-song_3_ref_img_3 = RefImg.create(title: "Slim Shady", img_url: "https://factmag-images.s3.amazonaws.com/wp-content/uploads/2015/04/Eminem021214-635x372.jpg", song: song_3, user: user_kanye, selected: false)
+song_3_ref_img_1 = RefImg.create(title: "Ocean", img_url: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/abstract-art-flaming-ocean-kathy-symonds.jpg", song: song_3, user: user_kanye, selected: true)
+song_3_ref_img_2 = RefImg.create(title: "Water", img_url: "https://www.trollart.com/wp-content/uploads/store-img/P/PlanetOcean.jpg", song: song_3, user: user_tim, selected: false)
+song_3_ref_img_3 = RefImg.create(title: "Fish", img_url: "https://i.pinimg.com/originals/b4/d0/87/b4d08752f0bd87658db860905261fd89.jpg", song: song_3, user: user_kanye, selected: false)
+song_3_ref_img_4 = RefImg.create(title: "more", img_url: "https://img5.goodfon.com/wallpaper/nbig/2/48/sea-waves-water-sky-orange-sky-artwork-digital-art-art.jpg", song: song_3, user: user_tim, selected: false)
+song_3_ref_img_5 = RefImg.create(title: "lake", img_url: "https://news.uga.edu/wp-content/uploads/2017/12/GMOA-1953_1277-Hokusai.jpg", song: song_3, user: user_kanye, selected: false)
+song_3_ref_img_6 = RefImg.create(title: "pool", img_url: "https://www.teamlab.art/images/pc-l/19809", song: song_3, user: user_tim, selected: false)
+song_3_ref_img_7 = RefImg.create(title: "Fishey", img_url: "https://ae01.alicdn.com/kf/HTB1ZadePFXXXXXxXVXXq6xXFXXX3/Claude-Monet-Water-Lilies-Canvas-Art-Printed-Painting-Reproduction-Canvas-Picture-Prints-Wall-Art-for-Home.jpg", song: song_3, user: user_kanye, selected: false)
+song_3_ref_img_8 = RefImg.create(title: "shark", img_url: "https://p0.pikrepo.com/preview/668/835/untitled.jpg", song: song_3, user: user_tim, selected: false)
+song_3_ref_img_9 = RefImg.create(title: "aqua", img_url: "hhttps://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWJ9QZcv0MF1fXtF1vcsdnzcQ_16lzY6Wrbw&usqp=CAU", song: song_3, user: user_kanye, selected: false)
 
 beat_1_song_3 = Beat.create(bpm: 145, key_sig: "F minor", selected: true, song: song_3, user: user_kanye, audio:'../app/audio/rockstar.mp3')
 beat_2_song_3 = Beat.create(bpm: 300, key_sig: "B# major", selected: false, song: song_3, user: user_mike_dean, audio:'../app/audio/rockstar.mp3')
