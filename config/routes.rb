cf8bde5c8272cffc50a336853f40edc712caaae3
github username <github email address>
2020-12-15 21:12:28 -0500

    auth_done

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :users
 
   post '/login', to: 'auth#create'
-  get './profile', to: 'users#profile'
+  get '/profile', to: 'users#profile'
 
   post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
